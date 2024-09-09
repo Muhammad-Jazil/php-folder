d54250a775a68ef67703ebac132d4d256784f148
Muhammad-jazil <jazilkhan111000@gmail.com>
2024-09-09 12:10:08 +0500

    9 sep

--- panel/php/query.php
+++ panel/php/query.php
@@ -9,10 +9,10 @@ if(isset($_POST['addcategory'])){
     $des = 'img/categories/'.$catimage;
     if($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp"){
         if(move_uploaded_file($catTmpname,$des)){
-            $query - $pdo > prepare("insert into categorys (catename,cateimage) values(:pn,:pi)");
-            $query >bindParam("pn",$catname);
-            $query >bindParam("pi",$catimage);
-            $query >execute();
+            $query = $pdo-> prepare("insert into categorys (catename,cateimage) values(:pn,:pi)");
+            $query ->bindParam("pn",$catname);
+            $query ->bindParam("pi",$catimage);
+            $query ->execute();
             echo "<script>alert('category added')</script>";
         }
     }
